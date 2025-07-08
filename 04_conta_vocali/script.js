b9772c97212a4873fb761f2ca384ea8fb717f8f6
MauroBorzi <mauroborzi91@gmail.com>
2025-07-08 16:42:13 +0200

    [add]contaVocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function contaVocali(text) {
+  const vocali = ["a", "e", "i", "o", "u"]
+  let numeroVocali = 0
+  for (let i = 0; i < text.length; i++) {
+      const vocale = text[i]
+    if (vocali.includes(vocale)) {
+      numeroVocali += +1
+    }
+  }
+  return numeroVocali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(contaVocali(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
