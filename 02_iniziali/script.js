3d21b15f2f7b1df765d32ba281233d869c8273b3
MauroBorzi <mauroborzi91@gmail.com>
2025-07-08 15:51:20 +0200

    [add]iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function letterExtracted(array){
+  const firstLetter = []
+  for(let i = 0; i < array.length; i++){
+      const word = array[i]
+    if(word.length > 0){
+      firstLetter.push(word[0])
+    }
+  }
+  return firstLetter
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(letterExtracted(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
